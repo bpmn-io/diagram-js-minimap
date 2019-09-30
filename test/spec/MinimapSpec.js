20cf2a08144900817f6eb66e3b531805b5e71c92
Philipp Fromme <philippfromme@outlook.com>
2019-09-30 13:33:24 +0200

    fix(minimap): do not log graphics not found
    Closes #38

--- test/spec/MinimapSpec.js
+++ test/spec/MinimapSpec.js
@@ -1,24 +1,26 @@
 /* global sinon */
 
 import {
-  remove as domRemove
+  attr as svgAttr,
+  remove as domRemove,
+  query as domQuery
 } from 'min-dom';
 
 import Diagram from 'diagram-js';
 
 import {
   bootstrapDiagram,
+  getDiagramJS,
   inject,
-  insertCSS,
-  getDiagramJS
+  insertCSS
 } from '../TestHelper';
 
 import minimapModule from '../../lib';
 
-import moveCanvasModule from 'diagram-js/lib/navigation/movecanvas';
-import zoomScrollModule from 'diagram-js/lib/navigation/zoomscroll';
 import modelingModule from 'diagram-js/lib/features/modeling';
+import moveCanvasModule from 'diagram-js/lib/navigation/movecanvas';
 import moveModule from 'diagram-js/lib/features/move';
+import zoomScrollModule from 'diagram-js/lib/navigation/zoomscroll';
 
 import minimapCSS from '../../assets/diagram-js-minimap.css';
 
@@ -38,53 +40,10 @@ var modelerModules = viewerModules.concat([
 
 describe('minimap', function() {
 
-  describe('in modeler', function() {
-
-    beforeEach(bootstrapDiagram({
-      modules: modelerModules
-    }));
+  this.timeout(20000);
 
 
-    it('should show', inject(function(canvas, modeling, elementFactory) {
-
-      var shapeA = elementFactory.createShape({
-        id: 'A',
-        width: 100,
-        height: 300
-      });
-
-      modeling.createShape(shapeA, { x: 100, y: 300 }, canvas.getRootElement());
-
-
-      var shapeB = elementFactory.createShape({
-        id: 'B',
-        width: 50,
-        height: 50
-      });
-
-      modeling.createShape(shapeB, { x: 800, y: 1200 }, canvas.getRootElement());
-
-
-      var shapeC = elementFactory.createShape({
-        id: 'C',
-        width: 300,
-        height: 300
-      });
-
-      modeling.createShape(shapeC, { x: 800, y: 100 }, canvas.getRootElement());
-
-      generateShapes(200, {
-        x: -200,
-        y: -50,
-        width: 3000,
-        height: 1000
-      });
-    }));
-
-  });
-
-
-  describe('in viewer', function() {
+  describe('viewer', function() {
 
     beforeEach(bootstrapDiagram({
       modules: viewerModules,
@@ -96,6 +55,7 @@ describe('minimap', function() {
 
     it('should show', inject(function(canvas, elementFactory) {
 
+      // when
       var shapeA = elementFactory.createShape({
         id: 'A',
         width: 100,
@@ -127,11 +87,17 @@ describe('minimap', function() {
       });
 
       canvas.addShape(shapeC, canvas.getRootElement());
+
+      // then
+      expectMinimapShape('A');
+      expectMinimapShape('B');
+      expectMinimapShape('C');
     }));
 
 
     it('should show single element', inject(function(canvas, elementFactory) {
 
+      // when
       var shapeA = elementFactory.createShape({
         id: 'A',
         width: 100,
@@ -141,6 +107,102 @@ describe('minimap', function() {
       });
 
       canvas.addShape(shapeA, canvas.getRootElement());
+
+      // then
+      expectMinimapShape('A');
+    }));
+
+  });
+
+
+  describe('modeler', function() {
+
+    beforeEach(bootstrapDiagram({
+      modules: modelerModules,
+      minimap: {
+        open: true
+      }
+    }));
+
+
+    it('should show', inject(function(canvas, modeling, elementFactory) {
+
+      // when
+      var shapeA = elementFactory.createShape({
+        id: 'A',
+        width: 100,
+        height: 300
+      });
+
+      modeling.createShape(shapeA, { x: 100, y: 300 }, canvas.getRootElement());
+
+
+      var shapeB = elementFactory.createShape({
+        id: 'B',
+        width: 50,
+        height: 50
+      });
+
+      modeling.createShape(shapeB, { x: 800, y: 1200 }, canvas.getRootElement());
+
+
+      var shapeC = elementFactory.createShape({
+        id: 'C',
+        width: 300,
+        height: 300
+      });
+
+      modeling.createShape(shapeC, { x: 800, y: 100 }, canvas.getRootElement());
+
+      var shapes = generateShapes(200, {
+        x: -200,
+        y: -50,
+        width: 3000,
+        height: 1000
+      });
+
+      // then
+      expectMinimapShape('A');
+      expectMinimapShape('B');
+      expectMinimapShape('C');
+
+      expectMinimapShapes(shapes);
+    }));
+
+
+    it('should update', inject(function(canvas, elementFactory, modeling) {
+
+      // given
+      var parent = elementFactory.createShape({
+        id: 'parent',
+        width: 100,
+        height: 100,
+        x: 100,
+        y: 100
+      });
+
+      var child = elementFactory.createShape({
+        id: 'child',
+        parent: parent,
+        width: 50,
+        height: 50,
+        x: 125,
+        y: 125
+      });
+
+      var rootElement = canvas.getRootElement();
+
+      modeling.createElements([ parent, child ], { x: 100, y: 100 }, rootElement);
+
+      expectMinimapShape('parent');
+      expectMinimapShape('child');
+
+      // when
+      modeling.resizeShape(parent, { x: 50, y: 50, width: 200, height: 100 });
+
+      // then
+      expectMinimapShape('parent');
+      expectMinimapShape('child');
     }));
 
   });
@@ -249,33 +311,69 @@ describe('minimap', function() {
 function generateShapes(count, viewport) {
 
   return getDiagramJS().invoke(function(canvas, elementFactory, modeling) {
+    var rootElement = canvas.getRootElement(),
+        shape;
 
-    var s;
+    var shapes = [];
 
     for (var i = 0; i < count; i++) {
-      s = elementFactory.createShape({
+      shape = elementFactory.createShape({
         id: 'shape' + i,
-        width: rnd(10, 300),
-        height: rnd(10, 200),
+        width: random(10, 300),
+        height: random(10, 200),
       });
 
-      modeling.createShape(s, {
-        x: rnd(viewport.x, viewport.width),
-        y: rnd(viewport.y, viewport.height)
-      }, canvas.getRootElement());
+      shapes.push(modeling.createShape(shape, {
+        x: random(viewport.x, viewport.width),
+        y: random(viewport.y, viewport.height)
+      }, rootElement));
     }
+
+    return shapes;
   });
 }
 
-function rnd(start, end) {
+function random(start, end) {
   return Math.round(Math.random() * (end - start) + start);
 }
 
 function triggerMouseEvent(type, gfx) {
-
   var event = document.createEvent('MouseEvent');
+
   event.initMouseEvent(type, true, true, window, 0, 0, 0, 80, 20, false, false, false, false, 0, null);
 
   return gfx.dispatchEvent(event);
 }
 
+function expectMinimapShape(id) {
+  getDiagramJS().invoke(function(elementRegistry, minimap) {
+    var element = elementRegistry.get(id);
+
+    expect(element).to.exist;
+
+    var minimapShape = domQuery('g#' + id, minimap._parent);
+
+    expect(minimapShape).to.exist;
+
+    var transform = svgAttr(minimapShape, 'transform');
+
+    var translate = transform.replace('translate(', '').replace(')', '').split(' ');
+
+    var x = parseInt(translate[0], 10),
+        y = parseInt(translate[1], 10);
+
+    var parentX = element.parent.x || 0,
+        parentY = element.parent.y || 0;
+
+    expect(x).to.equal(element.x - parentX);
+    expect(y).to.equal(element.y - parentY);
+  });
+}
+
+function expectMinimapShapes(shapes) {
+  shapes.forEach(function(shape) {
+    var id = shape.id;
+
+    expectMinimapShape(id);
+  });
+}
