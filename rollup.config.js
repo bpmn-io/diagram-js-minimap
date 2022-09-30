a9d36c02f803722f8dc53bf374c06f825e460923
Nico Rehwaldt <git_nikku@nixis.de>
2022-09-30 13:39:02 +0200

    chore: bump rollup dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { terser } from 'rollup-plugin-terser';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 import pkg from './package.json';
 
@@ -43,8 +43,8 @@ export default [
   {
     input: srcEntry,
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { file: pkg.main, format: 'cjs', exports: 'default' },
+      { file: pkg.module, format: 'es', exports: 'default' }
     ],
     external: [
       'diagram-js/lib/util/GraphicsUtil',
