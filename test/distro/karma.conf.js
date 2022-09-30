328120a8e2228e62f65f71452ed7f3292d6878a3
Nico Rehwaldt <git_nikku@nixis.de>
2022-09-30 13:35:51 +0200

    chore: bump test dependencies

--- test/distro/karma.conf.js
+++ test/distro/karma.conf.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // configures browsers to run test against
-// any of [ 'ChromeHeadless', 'Chrome', 'Firefox', 'IE', 'PhantomJS' ]
-var browsers = (process.env.TEST_BROWSERS || 'PhantomJS').split(',');
+// any of [ 'ChromeHeadless', 'Chrome', 'Firefox' ]
+var browsers = (process.env.TEST_BROWSERS || 'ChromeHeadless').split(',');
 
 // use puppeteer provided Chrome for testing
 process.env.CHROME_BIN = require('puppeteer').executablePath();
@@ -11,6 +11,7 @@ module.exports = function(karma) {
   karma.set({
 
     frameworks: [
+      'webpack',
       'mocha',
       'sinon-chai'
     ],
